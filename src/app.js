1901ce7be34c5f9aa8447c02c6efa20c86e4c463
Franco Dans <dansfranco17@gmail.com>
2021-11-18 19:44:09 -0300

    Primer commit navbar

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import { func } from "prop-types";
+import React from "react";
+import navbar from "./components/navbar/Navbar";
+
+
+
+export default class App extends React.Component{
+    render() {
+        <div className="App">
+            < Navbar />  
+               </div>
+    }
+}
+
+
+
